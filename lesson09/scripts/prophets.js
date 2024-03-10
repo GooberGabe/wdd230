73c846ff20f29e15b7d707fea6c1540a8cc1facc
Gabe <gdubs2000@gmail.com>
2024-03-09 23:14:16 -0700

    prophets activity

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -6,14 +6,25 @@ async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
     console.table(data.prophets);
+    displayProphets(data.prophets);
 }
 
 const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
+        console.log("YEET")
         const card = document.createElement("section");
         const fullName = document.createElement("h2");
         const portrait = document.createElement("img");
-        fullName.innerHTML = prophet.name + " " + prophet.lastName;
+        fullName.innerHTML = prophet.name + " " + prophet.lastname;
+        portrait.src = prophet.imageurl;
+        portrait.alt = portrait.innerHTML;
+        portrait.loading = 'lazy';
+        portrait.width = 340;
+        portrait.height = 440;
+
+        cards.appendChild(card);
+        card.appendChild(fullName);
+        card.appendChild(portrait);
     });
 }
 
