0a032ee8854197dd83f0da16d5593509fddabb27
Gabe <gdubs2000@gmail.com>
2024-03-13 18:32:54 -0600

    chamber directory

---
+++ chamber/scripts/members.js
@@ -0,0 +1,62 @@
+const baseURL = "https://goobergabe.github.io/wdd230/";
+const membersURL = "https://goobergabe.github.io/wdd230/chamber/data/members.json";
+
+async function getMembers() 
+{
+    const response = await fetch(membersURL);
+    const data = await response.json();
+    displayMembers(data);
+}
+
+function displayMembers(data) 
+{
+    for (let i = 0; i < data.members.length; i++) 
+    {
+
+        const member = data.members[i];
+        const sec = document.createElement("section");
+        sec.classList.append("card");
+        document.querySelector("#members").appendChild(sec);
+
+        for (let i2 = 0; i2 < member.images; i2++)
+        {
+            const imgUrl = member.images[i2];
+            const img = document.createElement("img");
+            img.src = imgUrl;
+            img.width = 350;
+            img.height = 350;
+            sec.appendChild(img);
+        }
+
+        const h2 = document.createElement("h2");
+        h2.innerHTML = member.name;
+        sec.appendChild(h2);
+
+        const p1 = document.createElement("p");
+        p1.innerHTML = member.address;
+        sec.appendChild(p1);
+
+        const p2 = document.createElement("p");
+        p2.innerHTML = member.phoneNumber;
+        sec.appendChild(p2);
+
+        const p3 = document.createElement("p");
+        p3.innerHTML = "Links: ";
+        sec.appendChild(p3);
+
+        const p4 = document.createElement("p");
+        p4.innerHTML = ["Basic","Bronze","Silver","Gold"][member.membership]+" Member";
+        sec.appendChild(p4);
+
+        for (let i2 = 0; i2 < member.urls; i2++)
+        {
+            const l = member.urls[i2];
+            const link = document.createElement("a");
+            link.href = l.url;
+            link.innerHTML = l.title+(i2 == member.urls.length - 1 ? "" : " | ");
+            p3.appendChild(link);
+        }
+    }
+}
+
+getMembers();
