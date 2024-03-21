f5c255dcbff3723b93f04719e092907d251da739
Gabe <gdubs2000@gmail.com>
2024-03-20 18:41:08 -0600

    fixed weather

--- scripts/windchill.js
+++ scripts/windchill.js
@@ -1,9 +1,20 @@
+const baseURL = "https://goobergabe.github.io/wdd230/";
+const weatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=39.57&lon=-84.23&appid=1184a90aee199bd2fdfa0ea4c5d68101";
+
+async function getWeather() 
+{
+    const response = await fetch(weatherURL);
+    const data = await response.json();
+    return data.main.temp - 273.15;
+}
+
 var tempDisplay = document.querySelector("#temp");
 var windDisplay = document.querySelector("#wind");
 var chillDisplay = document.querySelector("#chill");
 
 function getWindChill() {
-    temperature = parseInt(tempDisplay.innerHTML);
+    let w = getWeather();
+    temperature = parseInt(w);
     windSpeed = parseInt(windDisplay.innerHTML);
 
     v = windSpeed ** 0.16;
